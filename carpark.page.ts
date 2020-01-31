ba27398c385846f03d9635ae34b0b3c0afe6c644
yukta-99 <56963967+yukta-99@users.noreply.github.com>
2020-01-31 23:36:51 +0530

    Update carpark.page.ts

--- carpark.page.ts
+++ carpark.page.ts
@@ -1,7 +1,13 @@
+/*
+*Project Name: Smart Parking Aid System
+*Author List: Rutwik Patel, Pranav Garg
+*Filename: carpark.page.ts
+*Functions: No user defined functions
+*Global Variables: None
+*/
+
+
 import { Component, OnInit } from '@angular/core';
-
-
-
 @Component({
   selector: 'app-carpark',
   templateUrl: './carpark.page.html',

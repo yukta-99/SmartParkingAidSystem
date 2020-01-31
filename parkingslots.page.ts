d1df62751e6493644dd6b48c832a0be13c243210
yukta-99 <56963967+yukta-99@users.noreply.github.com>
2020-01-31 23:34:59 +0530

    Update parkingslots.page.ts

--- parkingslots.page.ts
+++ parkingslots.page.ts
@@ -1,26 +1,45 @@
+/*
+*Project Name: Smart Parking Aid System
+*Author List: Rutwik Patel, Pranav Garg
+*Filename: parkingslots.page.ts
+*Functions: initiate_data_request(), update_vacancy_data()
+*Global Variables: slotdata
+*/
+
 import { Component, OnInit } from '@angular/core';
+// Injecting the carpark.service.ts
 import { CarparkService } from 'src/app/carpark/carpark.service';
-
-
-
 @Component({
   selector: 'app-parkingslots',
   templateUrl: './parkingslots.page.html',
   styleUrls: ['./parkingslots.page.scss'],
 })
 export class ParkingslotsPage implements OnInit {
-  lam: string[] = [];
+  slotdata: string[] = [];
 
-  constructor(private some: CarparkService) { }
-
+  constructor(private ServiceInstance: CarparkService) { }
+  /*
+  *ngOnInit() is generated automatically when you generate a page in angular, the function ngOnInit() runs exactly once
+  *when the app starts
+  */
   ngOnInit() {
-    this.get_info('efasfeas');
-    this.whats();
+    this.initiate_data_request('Dummy String');
+    this.update_vacancy_data();
   }
-  get_info(msg: string) {
-    this.some.get_status('edfasf').subscribe();
+
+  /*
+  *initiate_data_request() uses the service instance of the carpark.service.ts inorder to call the function get_status()
+  *of carpark.service.ts which initialises the http.get request to get the slot vacancy data from the firebase real time database
+  */
+  initiate_data_request(msg: string) {
+    this.ServiceInstance.get_status('Dummy String').subscribe();
   }
-  whats() {
-    this.lam = this.some.retire();
+
+  /*
+  *update_vacancy_data() uses the service instance of the carpark.service.ts inorder to call the function return_data()
+  *of carpark.service.ts to update the slotdata string array which displays the vacancy status
+  */
+  update_vacancy_data() {
+    this.slotdata = this.ServiceInstance.return_data();
   }
 }

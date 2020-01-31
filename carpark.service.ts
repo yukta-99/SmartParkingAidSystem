6ab1d78c71fa81b06cff43d1a24117a11148620e
yukta-99 <56963967+yukta-99@users.noreply.github.com>
2020-01-31 23:36:21 +0530

    Update carpark.service.ts

--- carpark.service.ts
+++ carpark.service.ts
@@ -1,7 +1,16 @@
+/*
+*Project Name: Smart Parking Aid System
+*Author List: Rutwik Patel, Pranav Garg
+*Filename: carpark.service.ts
+*Functions: get_status(), return_data()
+*Global Variables: interface ' Placedata ', string array 'vacancydata'
+*/
+
+
+
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Status } from 'src/app/home/status.model';
-import { tap, take, map, delay, switchMap } from 'rxjs/operators';
+import { tap } from 'rxjs/operators';
 
 
 interface Placedata {
@@ -14,29 +23,36 @@ interface Placedata {
   providedIn: 'root'
 })
 export class CarparkService {
-  private fan: string[] = [
+  private vacancydata: string[] = [
     'Data',
     'Data',
     'Data'
   ];
 
   constructor(private http: HttpClient) { }
+
+  /*
+  *get_status() requests data of car park one from firebase realtime database and stores it in the string array ' vacancydata '
+  *Placedata is used to specify the data format in which we would like to map the response data coming from the firebase realtime database
+  */
   get_status(msg: string) {
     return this.http.get<{ [key: string]: Placedata }>('https://parking-cloud-1315b.firebaseio.com/car-park-one.json').pipe(tap(
       resData => {
         for (const key in resData) {
           if (true) {
-            // this.fan.push(resData[key].slot1, resData[key].slot2, resData[key].slot3);
-            this.fan[0] = resData[key].slot1;
-            this.fan[1] = resData[key].slot2;
-            this.fan[2] = resData[key].slot3;
-            // console.log(this.fan);
+            this.vacancydata[0] = resData[key].slot1;
+            this.vacancydata[1] = resData[key].slot2;
+            this.vacancydata[2] = resData[key].slot3;
           }
         }
       }
     ));
   }
-  retire() {
-    return this.fan;
+
+  /*
+  *return_data() sends the string array ' vacancydata ' when it is called
+  */
+  return_data() {
+    return this.vacancydata;
   }
 }
